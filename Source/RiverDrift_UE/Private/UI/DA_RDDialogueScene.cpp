f9ebe6184b368f5395b96db25d333da5405a08b7
RyanDavidsonDev <ryan.r.davidson.dev@gmail.com>
2025-09-16 20:58:07 -0600

    init data infrastructure for dialogue

---
+++ Source/RiverDrift_UE/Private/UI/DA_RDDialogueScene.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/DA_RDDialogueScene.h"
+
+FRDDialogueResponseOption::FRDDialogueResponseOption()
+{
+	UE_LOG(LogTemp, Log, TEXT("dialogue response option default constr called"))
+}
+
+FRDDialogueSlide::FRDDialogueSlide() 
+{
+	UE_LOG(LogTemp, Log, TEXT("dialogue slide default constr called"))
+
+}
---
+++ Source/RiverDrift_UE/Public/UI/DA_RDDialogueScene.h
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "Internationalization/Text.h"
+#include "DA_RDDialogueScene.generated.h"
+
+struct FRDDialogueResponseOption;
+struct FRDDialogueSlide;
+
+USTRUCT(BlueprintType)
+struct FRDDialogueResponseOption
+{
+	GENERATED_BODY()
+public:
+	FRDDialogueResponseOption();
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FText ButtonText;
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FText NextIntro;
+
+};
+
+USTRUCT(BlueprintType)
+struct  FRDDialogueSlide
+{
+	GENERATED_BODY()
+public:
+	FRDDialogueSlide();
+
+	FText DialogueIntro;
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FText DialogueMainBody;
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FText DialogueOutro;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FRDDialogueResponseOption RightResponse;
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FRDDialogueResponseOption LeftResponse;
+
+
+};
+
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class RIVERDRIFT_UE_API UDA_RDDialogueScene : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	UTexture2D* CharacterPortrait;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	TArray<FRDDialogueSlide> Slides;
+
+
+	
+};
